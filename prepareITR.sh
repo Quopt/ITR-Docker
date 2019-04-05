90f99e5ed6bbde05c2258cee0560a00416f107c0
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-04-05 17:52:52 +0200

    Update prepareITR.sh

--- prepareITR.sh
+++ prepareITR.sh
@@ -11,13 +11,15 @@ export EMAIL=certificates@testdimensions.com
 export DBPREFIX=ITR
 
 # Install
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update && sudo apt-get install -y git && sudo apt-get -y upgrade && sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-	sudo apt-key fingerprint 0EBFCD88
+    software-properties-common 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+sudo apt-key fingerprint 0EBFCD88
 # change this line depending on your cpu architecture. this assumes x86	
 sudo add-apt-repository \
      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
