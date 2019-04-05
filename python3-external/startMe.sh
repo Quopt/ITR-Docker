0908b01ecd59a24bd08d4fce8d956acb75d899c0
bgarssen@hotmail.com <bgarssen@hotmail.com>
2019-04-05 11:53:27 +0000

    commit

---
+++ python3-external/startMe.sh
@@ -0,0 +1,5 @@
+cd /etc/ITR/API
+pip install -r requirements.txt
+cd /etc/ITR/Public-API
+python3 application.py
+
---
+++ python3-internal/startMe.sh
@@ -0,0 +1,4 @@
+#!\bin\sh
+cd /etc/ITR/API
+pip install -r requirements.txt
+python3 application.py
