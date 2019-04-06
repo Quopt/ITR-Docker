cd0536c7d73a7b1f2a93471a38fa55025965b7ea
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-04-06 11:26:47 +0200

    Update letsEncrypt.sh

--- nginx/letsEncrypt.sh
+++ nginx/letsEncrypt.sh
@@ -16,7 +16,7 @@ mv /etc/nginx/nginx.conf /etc/certbot/nginx.conf
 mv /etc/certbot/nginx.conf.template.letsencrypt /etc/nginx/nginx.conf
 
 ./acme.sh --issue --nginx -d $WWW \
---key-file       /etc/nginx/ssl/certificate.key \
+--test --key-file       /etc/nginx/ssl/certificate.key \
 --fullchain-file /etc/nginx/ssl/certificate.crt \
 --reloadcmd     "service nginx force-reload"
 
