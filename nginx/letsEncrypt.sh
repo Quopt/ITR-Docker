d2b7b5d90f9e2cc219ff818d8f50002feb5028e5
root <root@vps671268.ovh.net>
2019-04-06 09:23:35 +0200

    commit

---
+++ nginx/letsEncrypt.sh
@@ -0,0 +1,17 @@
+cd /etc
+mkdir certbot
+cd certbot
+#./certbot-auto --nginx
+rm index.html
+rm acme.sh
+apk add curl openssl
+wget  -  https://get.acme.sh
+mv index.html acme.sh
+chmod +x acme.sh
+./acme.sh
+cd ~/.acme.sh
+
+acme.sh --install-cert -d $WWW \
+--key-file       /etc/nginx/ssl/certificate.key \
+--fullchain-file /etc/nginx/ssl/certificate.crt \
+--reloadcmd     "service nginx force-reload"
