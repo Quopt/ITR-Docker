012251a956cad0a818a6eb62272f3166d16c166f
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-04-06 12:58:32 +0200

    Update letsEncryptRenew.sh

--- nginx/letsEncryptRenew.sh
+++ nginx/letsEncryptRenew.sh
@@ -1,2 +1,8 @@
-./acme.sh --renew --nginx -d $WWW \
+apk add curl openssl
+wget  -  https://get.acme.sh
+mv index.html acme.sh
+chmod +x acme.sh
+./acme.sh --install --nocron
+
+~/.acme.sh/acme.sh --renew --nginx -d $WWW \
  --reloadcmd  "/usr/sbin/nginx -s reload"
