7e98a92395124af124c10f19d3a495b814255036
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-06-16 18:07:09 +0200

    Update letsEncryptRenew.sh
    Better compatibility with Let's encrypt

--- nginx/letsEncryptRenew.sh
+++ nginx/letsEncryptRenew.sh
@@ -4,5 +4,18 @@ mv index.html acme.sh
 chmod +x acme.sh
 ./acme.sh --install --nocron
 
-~/.acme.sh/acme.sh --renew --nginx --nocron -d $WWW \
- --reloadcmd  "/usr/sbin/nginx -s reload"
+export DOMAIN_PATH=/etc/nginx/ssl
+~/.acme.sh/acme.sh --renew --nginx -d $WWW \
+ --reloadcmd  "/usr/sbin/nginx -s reload" --home /etc/nginx 
+cd /etc/nginx/ssl
+file1=`md5sum certificate.crt | cut -d' ' -f1`
+file2=`md5sum $WWW.cer | cut -d' ' -f1`
+if [ ! "$file1" == "$file2" ]
+then
+ echo Installing new certificate and restarting nginx
+ cp $WWW.cer certificate.crt
+ /usr/sbin/nginx -s reload
+else
+ echo Certificate is not renewed ...
+fi
+
