11ddc8f5c53c696e82806c019b09dffbdea1968e
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-07-06 16:28:09 +0200

    Update letsEncryptRenew.sh

--- nginx/letsEncryptRenew.sh
+++ nginx/letsEncryptRenew.sh
@@ -5,10 +5,11 @@ chmod +x acme.sh
 ./acme.sh --install --nocron
 
 export DOMAIN_PATH=/etc/nginx/ssl
+echo $WWW
 for value in $WWW
 do
- ~/.acme.sh/acme.sh --renew --nginx -d $value \
-  --reloadcmd  "/usr/sbin/nginx -s reload" --home /etc/nginx 
+ echo $value
+ ~/.acme.sh/acme.sh --renew --nginx -d $value --reloadcmd  "/usr/sbin/nginx -s reload" --home /etc/nginx || true
 done
 cd /etc/nginx/ssl
 echo Restarting nginx
