2eddafe55b2797848a94dc2bcd4dbb88e737ecab
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-07-09 08:17:36 +0200

    Update buildNginx.sh

--- nginx/buildNginx.sh
+++ nginx/buildNginx.sh
@@ -1,16 +1,16 @@
 
+cd /etc/certbot/
+export WWWOLD=$WWW
 for value in $WWW
 do
- export WWWInstance=$value
- cat nginx.A > nginx.conf
- sed 's/$WWWInstance/'$WWWInstance'/g' nginx.B >> nginx.conf
- echo } >> nginx.conf
+ export WWW=$value
+ sed 's/$WWWInstance/server_name '$value';/g' nginx.A2 > nginx.conf
  cp nginx.conf /etc/nginx/nginx.conf
+ cat /etc/nginx/nginx.conf
  ./letsEncrypt.sh
 done
 
-
-
+export WWW=$WWWOLD
 cat nginx.A > nginx.conf
 for value in $WWW
 do
