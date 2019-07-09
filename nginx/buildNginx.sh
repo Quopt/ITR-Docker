1a6819d1feb0eb347e4ed053a0b36cbd26435ee9
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-07-09 07:21:05 +0200

    Update buildNginx.sh

--- nginx/buildNginx.sh
+++ nginx/buildNginx.sh
@@ -1,14 +1,22 @@
 cat nginx.A > nginx.conf
+for value in $WWW
+do
+ export WWWInstance=$value
+ cat nginx.A > nginx.conf
+ sed 's/$WWWInstance/'$WWWInstance'/g' nginx.B >> nginx.conf
+ echo } >> nginx.conf
+ cp nginx.conf /etc/nginx/nginx.conf
+ ./letsEncrypt.sh
+done
 
+
+
+cat nginx.A > nginx.conf
 for value in $WWW
 do
  export WWWInstance=$value
  sed 's/$WWWInstance/'$WWWInstance'/g' nginx.B >> nginx.conf
 done
-
 echo } >> nginx.conf
-
-
 cp nginx.conf /etc/nginx/nginx.conf
-./letsEncrypt.sh
 nginx -s reload
