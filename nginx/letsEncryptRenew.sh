94cd7bfa203f741f3374d8754872717767fa22a5
bgarssen <31415742+bgarssen@users.noreply.github.com>
2019-12-26 11:57:47 +0100

    Update letsEncryptRenew.sh
    Fix for acme.sh removing key files

--- nginx/letsEncryptRenew.sh
+++ nginx/letsEncryptRenew.sh
@@ -7,12 +7,18 @@ chmod +x acme.sh
 ./acme.sh --install --nocron
 
 export DOMAIN_PATH=/etc/nginx/ssl
+mkdir /etc/nginx/ssl/keybkp || true
+cp /etc/nginx/ssl/*.key /etc/nginx/ssl/keybkp
+
 echo $WWW
 for value in $WWW
 do
  echo $value
- ~/.acme.sh/acme.sh --renew --nginx -d $value --reloadcmd  "/usr/sbin/nginx -s reload" --home /etc/nginx || true
+ ~/.acme.sh/acme.sh --renew --nginx -d $value --reloadcmd  "/usr/sbin/nginx -s reload" --home /etc/nginx $@ || true
 done
+
 cd /etc/nginx/ssl
+cp keybkp/*.key .
+
 echo Restarting nginx
 /usr/sbin/nginx -s reload
