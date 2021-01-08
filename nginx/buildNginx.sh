7e7763980852ae5a3ec5c74aa08cd444338eb80c
Bart <31415742+bgarssen@users.noreply.github.com>
2021-01-08 18:58:53 +0100

    Fix for in container updating of certificates

--- nginx/buildNginx.sh
+++ nginx/buildNginx.sh
@@ -1,16 +1,13 @@
-
 cd /etc/certbot/
+# acquire certificates
 export WWWOLD=$WWW
-for value in $WWWOLD
-do
- export WWW=$value
- sed 's/$WWWInstance/server_name '$value';/g' nginx.A2 > nginx.conf
- cp nginx.conf /etc/nginx/nginx.conf
- cat /etc/nginx/nginx.conf
+if [ "$1" == "FIRSTSTART" ]
+then
  ./letsEncrypt.sh
  cd /etc/certbot/
-done
+fi 
 
+# build nginx production configuration
 export WWW=$WWWOLD
 cat nginx.A > nginx.conf
 for value in $WWW
@@ -21,10 +18,3 @@ done
 echo } >> nginx.conf
 cp nginx.conf /etc/nginx/nginx.conf
 nginx -s reload
-
-export temp=s/'$WWW'/$WWW/g
-export temp=$(echo `expr "'$temp'"`)
-export temp=$(echo sed $temp nginx.conf.template.letsencrypt.org)
-echo $temp > temp.sh
-chmod +x temp.sh
-./temp.sh > nginx.conf.template.letsencrypt
